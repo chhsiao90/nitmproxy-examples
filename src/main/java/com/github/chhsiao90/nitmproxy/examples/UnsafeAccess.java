c0f104f43e2dce44e0d69cb13751b43d95447dc9
Edward Hsiao <edward@migo.io>
2021-09-01 14:18:47 +0800

    Add example for unsafe access

---
+++ src/main/java/com/github/chhsiao90/nitmproxy/examples/UnsafeAccess.java
@@ -0,0 +1,17 @@
+package com.github.chhsiao90.nitmproxy.examples;
+
+import com.github.chhsiao90.nitmproxy.NitmProxyConfig;
+import com.github.chhsiao90.nitmproxy.tls.SimpleUnsafeAccessSupport;
+import com.google.common.collect.ImmutableList;
+
+public class UnsafeAccess {
+
+  public static void main(String[] args) throws Exception {
+    SimpleUnsafeAccessSupport unsafeAccessSupport = new SimpleUnsafeAccessSupport();
+
+    NitmProxyConfig config = new NitmProxyConfig();
+    config.setUnsafeAccessSupport(unsafeAccessSupport);
+    config.setHttpListeners(ImmutableList.of(unsafeAccessSupport.getInterceptor()));
+    Bootstrap.start(config);
+  }
+}
